1d73f1f9f9dc4e124e537b4af7609c13d4505da4
devj2005 <vrindevj@gmail.com>
2025-09-21 15:40:25 +0300

    changes made and 2fa using qr code added and signup page added

---
+++ Config.php
@@ -0,0 +1,17 @@
+<?php
+// Database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'auth_db');
+define('DB_USER', 'auth_user');
+define('DB_PASS', 'strong_password');
+
+// SMTP (Gmail)
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USER', 'vrindevj@@gmail.com');
+define('SMTP_PASS', 'qiyc qaed gdmu ehtm');
+define('SITE_URL', 'http://localhost/IAP2.2X');
+
+// Start session
+session_start();
+?>
